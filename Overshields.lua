23e86ff13b553ecd7f83004ab4cbf59db7c60c86
Sean Treadway <wowenth@gmail.com>
2024-07-27 09:01:00 -0700

    Fix Player/Target/Party frames
    10.2.5 introduced StatusBarOverlaySegment for heal prediction and
    absorbs on UnitFrame frames.
    
    CompactUnitFrame frames still use the original overlays.

--- Overshields.lua
+++ Overshields.lua
@@ -1,151 +1,125 @@
-local ABSORB_GLOW_ALPHA = 0.6;
-local ABSORB_GLOW_OFFSET = -5;
+local ABSORB_GLOW_ALPHA = 0.6
+local ABSORB_GLOW_OFFSET = -7
 
-hooksecurefunc("UnitFrame_Update", function(frame)
-    local absorbBar = frame.totalAbsorbBar;
-    if not absorbBar or absorbBar:IsForbidden() then
-        return
-    end
-
-    local absorbOverlay = frame.totalAbsorbBarOverlay;
-    if not absorbOverlay or absorbOverlay:IsForbidden() then
-        return
-    end
-
-    local healthBar = frame.healthbar;
-    if not healthBar or healthBar:IsForbidden() then
-        return
-    end
-
-    absorbOverlay:SetParent(healthBar);
-    absorbOverlay:ClearAllPoints(); -- we'll be attaching the overlay on heal prediction update.
-
-    local absorbGlow = frame.overAbsorbGlow;
-    if absorbGlow and not absorbGlow:IsForbidden() then
-        absorbGlow:ClearAllPoints();
-        absorbGlow:SetPoint("TOPLEFT", absorbOverlay, "TOPLEFT", ABSORB_GLOW_OFFSET, 0);
-        absorbGlow:SetPoint("BOTTOMLEFT", absorbOverlay, "BOTTOMLEFT", ABSORB_GLOW_OFFSET, 0);
-        absorbGlow:SetAlpha(ABSORB_GLOW_ALPHA);
-    end
-end)
-
-hooksecurefunc("CompactUnitFrame_UpdateAll", function(frame)
-    local absorbBar = frame.totalAbsorb;
-    if not absorbBar or absorbBar:IsForbidden() then
-        return
-    end
-
-    local absorbOverlay = frame.totalAbsorbOverlay;
-    if not absorbOverlay or absorbOverlay:IsForbidden() then
-        return
-    end
-
-    local healthBar = frame.healthBar;
-    if not healthBar or healthBar:IsForbidden() then
-        return
-    end
-
-    absorbOverlay:SetParent(healthBar);
-    absorbOverlay:ClearAllPoints(); -- we'll be attaching the overlay on heal prediction update.
-
-    local absorbGlow = frame.overAbsorbGlow;
-    if absorbGlow and not absorbGlow:IsForbidden() then
-        absorbGlow:ClearAllPoints();
-        absorbGlow:SetPoint("TOPLEFT", absorbOverlay, "TOPLEFT", ABSORB_GLOW_OFFSET, 0);
-        absorbGlow:SetPoint("BOTTOMLEFT", absorbOverlay, "BOTTOMLEFT", ABSORB_GLOW_OFFSET, 0);
-        absorbGlow:SetAlpha(ABSORB_GLOW_ALPHA);
-    end
-end)
-
-hooksecurefunc("UnitFrameHealPredictionBars_Update", function(frame)
-    local absorbBar = frame.totalAbsorbBar;
-    if not absorbBar or absorbBar:IsForbidden() then
-        return
-    end
-
-    local absorbOverlay = frame.totalAbsorbBarOverlay;
-    if not absorbOverlay or absorbOverlay:IsForbidden() then
-        return
-    end
-
-    local healthBar = frame.healthbar;
-    if not healthBar or healthBar:IsForbidden() then
-        return
-    end
-
-    local _, maxHealth = healthBar:GetMinMaxValues();
-    if maxHealth <= 0 then
-        return
-    end
-
-    local totalAbsorb = UnitGetTotalAbsorbs(frame.unit) or 0;
-    if totalAbsorb > maxHealth then
-        totalAbsorb = maxHealth;
-    end
-
-    if totalAbsorb > 0 then -- show overlay when there's a positive absorb amount
-        if absorbBar:IsShown() then -- If absorb bar is shown, attach absorb overlay to it; otherwise, attach to health bar.
-            absorbOverlay:SetPoint("TOPRIGHT", absorbBar, "TOPRIGHT", 0, 0);
-            absorbOverlay:SetPoint("BOTTOMRIGHT", absorbBar, "BOTTOMRIGHT", 0, 0);
-        else
-            absorbOverlay:SetPoint("TOPRIGHT", healthBar, "TOPRIGHT", 0, 0);
-            absorbOverlay:SetPoint("BOTTOMRIGHT", healthBar, "BOTTOMRIGHT", 0, 0);
+hooksecurefunc(
+    "UnitFrameHealPredictionBars_Update",
+    function(frame)
+        local absorbBar = frame.totalAbsorbBar
+        if not absorbBar or absorbBar:IsForbidden() then
+            return
         end
 
-        local totalWidth, totalHeight = healthBar:GetSize();
-        local barSize = totalAbsorb / maxHealth * totalWidth;
-
-        absorbOverlay:SetWidth(barSize);
-        absorbOverlay:SetTexCoord(0, barSize / absorbOverlay.tileSize, 0, totalHeight / absorbOverlay.tileSize);
-        absorbOverlay:Show();
-
-        -- frame.overAbsorbGlow:Show();	--uncomment this if you want to ALWAYS show the glow to the left of the shield overlay
-    end
-
-end)
-
-hooksecurefunc("CompactUnitFrame_UpdateHealPrediction", function(frame)
-    local absorbBar = frame.totalAbsorb;
-    if not absorbBar or absorbBar:IsForbidden() then
-        return
-    end
-
-    local absorbOverlay = frame.totalAbsorbOverlay;
-    if not absorbOverlay or absorbOverlay:IsForbidden() then
-        return
-    end
-
-    local healthBar = frame.healthBar;
-    if not healthBar or healthBar:IsForbidden() then
-        return
-    end
-
-    local _, maxHealth = healthBar:GetMinMaxValues();
-    if maxHealth <= 0 then
-        return
-    end
-
-    local totalAbsorb = UnitGetTotalAbsorbs(frame.displayedUnit) or 0;
-    if totalAbsorb > maxHealth then
-        totalAbsorb = maxHealth;
-    end
-
-    if totalAbsorb > 0 then -- show overlay when there's a positive absorb amount
-        if absorbBar:IsShown() then -- If absorb bar is shown, attach absorb overlay to it; otherwise, attach to health bar.
-            absorbOverlay:SetPoint("TOPRIGHT", absorbBar, "TOPRIGHT", 0, 0);
-            absorbOverlay:SetPoint("BOTTOMRIGHT", absorbBar, "BOTTOMRIGHT", 0, 0);
-        else
-            absorbOverlay:SetPoint("TOPRIGHT", healthBar, "TOPRIGHT", 0, 0);
-            absorbOverlay:SetPoint("BOTTOMRIGHT", healthBar, "BOTTOMRIGHT", 0, 0);
+        local absorbGlow = frame.overAbsorbGlow
+        if not absorbGlow or absorbGlow:IsForbidden() then
+            return
         end
 
-        local totalWidth, totalHeight = healthBar:GetSize();
-        local barSize = totalAbsorb / maxHealth * totalWidth;
+        local healthBar = frame.healthbar
+        if not healthBar or healthBar:IsForbidden() then
+            return
+        end
 
-        absorbOverlay:SetWidth(barSize);
-        absorbOverlay:SetTexCoord(0, barSize / absorbOverlay.tileSize, 0, totalHeight / absorbOverlay.tileSize);
-        absorbOverlay:Show();
+        -- From StatusBar
+        local healthBarTexture = healthBar:GetStatusBarTexture()
+
+        -- From StatusBarOverlaySegmentTemplate
+        local absorbFillTexture = absorbBar.Fill
+        local absorbFillMaskTexture = absorbBar.FillMask
+
+        local curHealth = healthBar:GetValue()
+        if curHealth <= 0 then
+            return
+        end
+
+        local _, maxHealth = healthBar:GetMinMaxValues()
+        if maxHealth <= 0 then
+            return
+        end
+
+        local totalAbsorb = UnitGetTotalAbsorbs(frame.unit) or 0
+        if totalAbsorb <= 0 then
+            return
+        end
+
+        local effectiveHealth = curHealth + totalAbsorb
+        if effectiveHealth <= maxHealth then
+            -- normal - fill health deficit with absorb bar
+            absorbGlow:ClearAllPoints()
+            absorbGlow:SetPoint("TOPLEFT", healthBarTexture, "TOPRIGHT", ABSORB_GLOW_OFFSET, 0)
+            absorbGlow:SetPoint("BOTTOMLEFT", healthBarTexture, "BOTTOMRIGHT", ABSORB_GLOW_OFFSET, 0)
+            absorbGlow:SetAlpha(ABSORB_GLOW_ALPHA)
+        else
+            -- overshield - fill health deficit and remaining absorb percentage into health bar
+            local xOffset = (maxHealth / effectiveHealth) - 1
+            absorbBar:UpdateFillPosition(healthBar:GetStatusBarTexture(), totalAbsorb, xOffset)
+
+            -- anchor overabsorb glow left into health bar
+            absorbGlow:ClearAllPoints()
+            absorbGlow:SetPoint("TOPLEFT", absorbFillMaskTexture, "TOPLEFT", ABSORB_GLOW_OFFSET, 0)
+            absorbGlow:SetPoint("BOTTOMLEFT", absorbFillMaskTexture, "BOTTOMLEFT", ABSORB_GLOW_OFFSET, 0)
+            absorbGlow:SetAlpha(ABSORB_GLOW_ALPHA)
+        end
+    end
+)
+
+hooksecurefunc(
+    "CompactUnitFrame_UpdateHealPrediction",
+    function(frame)
+        local absorbBar = frame.totalAbsorb
+        if not absorbBar or absorbBar:IsForbidden() then
+            return
+        end
+
+        local absorbOverlay = frame.totalAbsorbOverlay
+        if not absorbOverlay or absorbOverlay:IsForbidden() then
+            return
+        end
+
+        local absorbGlow = frame.overAbsorbGlow
+        if not absorbGlow or absorbGlow:IsForbidden() then
+            return
+        end
+
+        local healthBar = frame.healthBar
+        if not healthBar or healthBar:IsForbidden() then
+            return
+        end
+
+        local _, maxHealth = healthBar:GetMinMaxValues()
+        if maxHealth <= 0 then
+            return
+        end
+
+        local totalAbsorb = UnitGetTotalAbsorbs(frame.displayedUnit) or 0
+        if totalAbsorb > maxHealth then
+            totalAbsorb = maxHealth
+        end
+
+        if totalAbsorb > 0 then -- show overlay when there's a positive absorb amount
+            absorbOverlay:SetParent(healthBar)
+            absorbOverlay:ClearAllPoints() -- we'll be attaching the overlay on heal prediction update.
+
+            if absorbBar:IsShown() then -- If absorb bar is shown, attach absorb overlay to it; otherwise, attach to health bar.
+                absorbOverlay:SetPoint("TOPRIGHT", absorbBar, "TOPRIGHT", 0, 0)
+                absorbOverlay:SetPoint("BOTTOMRIGHT", absorbBar, "BOTTOMRIGHT", 0, 0)
+            else
+                absorbOverlay:SetPoint("TOPRIGHT", healthBar, "TOPRIGHT", 0, 0)
+                absorbOverlay:SetPoint("BOTTOMRIGHT", healthBar, "BOTTOMRIGHT", 0, 0)
+            end
+
+            local totalWidth, totalHeight = healthBar:GetSize()
+            local barSize = totalAbsorb / maxHealth * totalWidth
+
+            absorbOverlay:SetWidth(barSize)
+            absorbOverlay:SetTexCoord(0, barSize / absorbOverlay.tileSize, 0, totalHeight / absorbOverlay.tileSize)
+            absorbOverlay:Show()
+
+            absorbGlow:ClearAllPoints()
+            absorbGlow:SetPoint("TOPLEFT", absorbOverlay, "TOPLEFT", ABSORB_GLOW_OFFSET, 0)
+            absorbGlow:SetPoint("BOTTOMLEFT", absorbOverlay, "BOTTOMLEFT", ABSORB_GLOW_OFFSET, 0)
+            absorbGlow:SetAlpha(ABSORB_GLOW_ALPHA)
 
         -- frame.overAbsorbGlow:Show();	--uncomment this if you want to ALWAYS show the glow to the left of the shield overlay
+        end
     end
-end)
+)
--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Overshields
 
-Overshields is a fork of the abandoned addon DerangementShieldMeters by Derangement.
+Overshields is an original fork of the abandoned addon DerangementShieldMeters by Derangement. Updates by Casper Rogild Storm and Enth.
+
 Ever wonder how much shield is still left, several hits after you cast Power Word: Shield on someone with full health, or use Death Strike to heal yourself as a tank?
+
 Overshields will provide permanent feedback on total shielding amounts regardless of whether the shielded person's HP is full or not.
 
-Works with blizzard's default Player, Target, Focus, Party, and Raid frames, as well as any custom frames that use their health prediction functions.
+Works with Blizzard's default Player, Target, Focus, Party, and Raid frames.
 
-![overshields](./screenshot.png)
+![overshields raid](./screenshot.png)
+
+![overshields target](./target.png)
 
 ## Installation
 
